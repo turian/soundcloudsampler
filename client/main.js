f7be96f08640b39f5e58659b06f6740ae405b839
Joseph Turian <turian@gmail.com>
2014-04-26 21:03:52 -0700

    Added soundcloud login

--- client/main.js
+++ client/main.js
@@ -8,11 +8,33 @@ var audio5js = new Audio5js({
     player = this;
 
     SC.initialize({
-      client_id: "c77badb5b68bb339edd72aeb1048266c"
+      client_id: "c77badb5b68bb339edd72aeb1048266c",
+      redirect_uri: Meteor.absoluteUrl("server/soundcloud-callback.html")
     });
 
     player.on('canplay', function(){
       player.play();
+    });
+  }
+});
+
+console.log(Meteor.absoluteUrl("server/soundcloud-callback.html"));
+
+Template.login.events({
+  'click': function(e) {
+    e.preventDefault();
+    SC.connect(function() {
+      // if SoundCloud login is valid, simply 'log in' locally with the SC user's ID
+      // it's a hack, but for now simple meteor-handled, SC-validated account & session management should suffice for now
+      SC.get('/me', function(user) {
+/*
+        Meteor.loginWithPassword(user.username, user.id, function (error) {
+          if (error)
+            // create a new 'user' if they don't exist
+            Accounts.createUser({username: user.username, password: user.id});
+        });
+*/
+      });
     });
   }
 });
