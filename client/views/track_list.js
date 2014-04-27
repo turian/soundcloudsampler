f2df7743c11fdeb1fd63be7e8c85bc361ece00ee
Joseph Turian <turian@gmail.com>
2014-04-26 22:39:36 -0700

    Small cleanup

--- client/views/track_list.js
+++ client/views/track_list.js
@@ -1,6 +1,5 @@
 Template.trackList.events({
   "click": function(e) {
-    alert("click");
-    player.load("http://api.soundcloud.com/tracks/146663487/stream?client_id=c77badb5b68bb339edd72aeb1048266c");
+    player.load("http://api.soundcloud.com/tracks/146289130/stream?client_id=c77badb5b68bb339edd72aeb1048266c");
   }
 });
