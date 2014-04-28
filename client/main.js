6c4391ad8ae3f95702c033f7cb2668be271ed98e
Joseph Turian <turian@gmail.com>
2014-04-27 23:55:58 -0700

    Fixed playback on iOS

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+var currentSound;
 Meteor.startup(function() {
   Session.set("isPlaying", false);          // Because there's no autoplay until a user event
   Session.set("isHoldingPlay", false);
@@ -6,30 +7,14 @@ Meteor.startup(function() {
     client_id: "c77badb5b68bb339edd72aeb1048266c",
     redirect_uri: Meteor.absoluteUrl("server/soundcloud-callback.html")
   });
-});
 
-Template.login.events({
-  'click': function(e) {
-    e.preventDefault();
-    SC.connect(function() {
-      // if SoundCloud login is valid, simply 'log in' locally with the SC user's ID
-      // it's a hack, but for now simple meteor-handled, SC-validated account & session management should suffice for now
-      SC.get('/me', function(user) {
-/*
-        Meteor.loginWithPassword(user.username, user.id, function (error) {
-          if (error)
-            // create a new 'user' if they don't exist
-            Accounts.createUser({username: user.username, password: user.id});
-        });
-*/
-      });
-    });
-  }
+  // Set up the first track to play
+  SC.stream("/tracks/293", function(sound){
+    currentSound = sound;
+  });
 });
 
 startTrack = function(trackUrl) {
-  SC.stream("/tracks/293", function(sound){
-    Session.set("isPlaying", true);
-    sound.play();
-  });
+  Session.set("isPlaying", true);
+  currentSound.play();
 }
